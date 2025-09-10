a220898067ceb21f010a9e6d3dc19f3752d96884
lux-QAQ <1098346640@qq.com>
2025-09-10 14:05:33 +0800

    fix(Astro Config): fix path err

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astronaut.github.io',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
